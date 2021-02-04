120f02b744e80e04a18c7fb8f408addaaf183180
Emil Patrick <wangilisasi@gmail.com>
2021-02-04 12:28:56 +0300

    random fix

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const User=require("../model/user")
 router.post("/", upload.single("image"), async(req,res)=>{
     try{
         const result=await cloudinary.uploader.upload(req.file.path);
+        console.log(req.file.path)
         //res.json(result);
 
         //Create new user
