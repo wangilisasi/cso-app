1f89d324c560e9e5148183efc7af24cc05f9dfd5
Emil Patrick <wangilisasi@gmail.com>
2021-02-04 10:37:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+
+const app=express();
+
+//Connect database
+mongoose.connect(process.env.MONGO_URI,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true
+}).then(()=>console.log("MongoDB connected"))
+.catch((err)=>console.log(err))
+
+//Middleware to parse request body
+app.use(express.json());
+
+//Route
+app.use("/user",require("./routes/user"))
+
+
+
+
+
+
+
+
+
+app.listen(5000,()=>console.log("Server started at port 5000"));
---
+++ routes/user.js
@@ -0,0 +1,69 @@
+const router=require("express").Router();
+const cloudinary=require("../utils/cloudinary");
+const upload=require("../utils/multer");
+const User=require("../model/user")
+
+router.post("/", upload.single("image"), async(req,res)=>{
+    try{
+        const result=await cloudinary.uploader.upload(req.file.path);
+        //res.json(result);
+
+        //Create new user
+        let user=new User({
+            name:req.body.name,
+            avatar:result.secure_url,
+            cloudinary_id:result.public_id
+        });
+        //Save user
+        await user.save();
+        res.json(user);
+    }catch (err){
+        console.log(err)
+    }
+})
+
+router.get("/",async(req,res)=>{
+    try{
+        let user=await User.find();
+        res.json(user)
+    }catch (err){
+        console.log(err);
+    }
+})
+
+router.delete("/:id", async(req,res)=>{
+    try{
+        //Find user by ID
+        let user=await User.findById(req.params.id);
+        //Delete image from cloudinary
+        await cloudinary.uploader.destroy(user.cloudinary_id); //destroy takes the cloudinary public ID
+        //Delete user from DB
+        await user.remove()
+        res.json(user);
+    }catch (err){
+        console.log(err);
+    }
+})
+
+router.put("/:id",upload.single("image"),async (req,res)=>{
+    try{
+        let user=await User.findById(req.params.id);
+        //first delete existing image
+        await cloudinary.uploader.destroy(user.cloudinary_id);
+        //then upload the new file
+        const result=await cloudinary.uploader.upload(req.file.path);
+        //then create a request body
+        const data={
+            name:req.body.name||user.name,  //if you provide a new name. otherwise it will use the name in the database
+            avatar:result.secure_url||user.avatar,  //use new image if updated otherwise use the old one
+            cloudinary_id:result.public_id||user.cloudinary_id,
+        };
+
+        user=await User.findByIdAndUpdate(req.params.id,data,{new:true});
+        res.json(user);
+    }catch(err){
+        console.log(err)
+    }
+})
+
+module.exports=router;
