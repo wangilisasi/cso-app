607df24f63128ba22b21e75cb9dfd9b23454bf25
Emil Patrick <wangilisasi@gmail.com>
2021-02-20 17:28:10 +0300

    used .env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const dotenv=require("dotenv").config();
 const express=require("express");
 const mongoose=require("mongoose");
 const routes=require("./routes/user")
-const dotenv=require("dotenv");
-dotenv.config();
+
+
 
 const app=express();
 
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary=require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: "wangilisasi",
-    api_key: "647743781872877",
-    api_secret: "8CXjUTC9D2x_CZfybFimVZcEuWc"
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
 });
 
 module.exports=cloudinary;
