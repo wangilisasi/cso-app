9f91ced1b3e3616ff4a8ddc201d418ce8542fc9c
Emil Patrick <wangilisasi@gmail.com>
2021-02-26 12:25:31 +0300

    rand tshoot

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post("/login",(req,res)=>{
             else{
                 bcrypt.compare(req.body.password,user.password,(error,match)=>{
                     if(error) res.status(500).json(error)
-                    else if(match) res.status(200).json({token:generateToken(user)})
+                    else if(match) res.status(200).json({user,token:generateToken(user)})
                     else res.status(403).json({error:"passwords do not match"})
                 })
             }
