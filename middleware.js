ac822d16e6cc13aa5effc0dfe50006b96bfcf186
Emil Patrick <wangilisasi@gmail.com>
2021-03-04 13:41:55 +0300

    chenged srevr errors

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ const tokenSecret="my-token-secret"
 exports.verify=(req,res,next)=>{
     const token=req.headers.authorization
     console.log(token)
-    if(!token) res.status(403).json({error:"please provide a token"})
+    if(!token) res.status(401).json({error:"please provide a token"})
     else{
         jwt.verify(token.split(" ")[1],tokenSecret,(err,value)=>{
-            if(err) res.status(500).json({error:"failed to authenticate token"})
+            if(err) res.status(401).json({error:"failed to authenticate token"})
             if(value){
                 req.user=value.data
             }
